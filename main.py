0e460cdef6350cc0ff59767b6351095c57ab82cf
Eduh gitahi <edwingitahi11@gmail.com>
2024-10-15 16:28:27 +0300

    first commit

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from scrape import (
     scrape_website,
     split_dom_content,
     clean_body_content,
-    extract_body_content,
-    
+    extract_body_content,   
 )
+from parse import parse_with_ollama
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter website URL: ")
@@ -35,3 +35,5 @@ if "dom_content" in st.session_state:
             st.write("Parsing the content")
             
             dom_chunks = split_dom_content(st.session_state.dom_content)
+            result = parse_with_ollama(dom_chunks, parse_description)
+            st.write(result)
